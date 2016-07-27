d0308b103fab59d7da8f77f38d58f73e21e1ec42
Pablo Cortez <pabloecortez07@gmail.com>
2016-07-26 23:04:11 -0700

    New site

---
+++ script.js
@@ -0,0 +1,7 @@
+$(function() {
+    $(".element").typed({
+        strings: ["Hi,^1000 I'm Pablo.", "Here's a little information about me:^2000"],
+        loop: true,
+        contentType: 'html' // or 'text'
+    });
+});
