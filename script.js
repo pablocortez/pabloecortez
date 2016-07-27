6ccc24a4a8856a2b4c7d3d569add3c9de3dc7f75
Pablo Cortez <pabloecortez07@gmail.com>
2016-07-27 13:02:14 -0700

    Optimizing for mobile

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
     $(".element").typed({
-        strings: ["Hi,^1000 I'm Pablo.", "Here's a little information about me:^2000"],
+        strings: ["Hi,^1000 I'm Pablo.", "Here's a little info about me:^2000"],
         loop: true,
         contentType: 'html' // or 'text'
     });
